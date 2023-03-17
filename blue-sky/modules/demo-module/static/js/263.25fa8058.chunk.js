b30c414e1eb14aeca2da5bbb2576fb6d6c91baa3
Anil Devan <adevan@greendotcorp.com>
2023-03-17 14:43:03 -0400

    bluesky demo

---
+++ blue-sky/modules/demo-module/static/js/263.25fa8058.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktfin_ui_project_template=self.webpackChunktfin_ui_project_template||[]).push([[263],{26263:function(e){e.exports=JSON.parse('{"loading.error.title":"Something went wrong","loading.error.msg":"Please try again later","activation.success.headingText":"Download and log into the app to access your account"}')}}]);
